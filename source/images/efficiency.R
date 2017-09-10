3b6bfad1fd190bee302e9f3479e5c5a80bd2f623
Matteo Abis <gmatteo..abis@gmail.com>
2017-09-10 21:26:07 +0200

    new stability figure

--- source/images/efficiency.R
+++ source/images/efficiency.R
@@ -2,23 +2,27 @@
 
 library(ggplot2)
 library(data.table)
+library(scales)
 
-dt = fread("efficiency_table.csv")
+dt = fread("visibility.csv")
+setnames(dt, "V1", "time")
+setnames(dt, "V2", "visibility")
 print(dt)
 
-molten = melt(dt, id.vars="energy")
-print(molten)
+time_origin = dt[1, time]
+dt[, time := as.POSIXct(time - time_origin, origin="1970-01-01")]
 
-plot = ggplot(molten, aes(x=energy, y=value, color=variable)) +
+plot = ggplot(dt, aes(x=time, y=visibility)) +
     geom_line() +
-    xlab("energy (keV)") +
-    ylab("quantum efficiency") +
-    labs(color="material")
+    scale_x_datetime(labels=date_format("%H")) +
+    scale_y_continuous(limits=c(0.05, 0.1)) +
+    xlab("time (hours)") +
+    ylab("median visibility")
 
 X11()
 print(plot)
-width = 10
-factor = 0.618
+width = 6
+factor = 0.75
 height = width * factor
-ggsave("efficiency.png", plot, width=width, height=height, dpi=300)
+ggsave("visibility_stable.png", plot, width=width, height=height, dpi=300)
 invisible(readLines(con="stdin", 1))
